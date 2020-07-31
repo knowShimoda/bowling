ec8d950270e4c2ed01215b8110510cd59aa61a52
EC2 Default User <ec2-user@ip-172-31-11-147.us-east-2.compute.internal>
2020-07-31 04:48:23 +0000

    bowling_test08

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -31,13 +31,7 @@ class Bowling
        @scores.each.with_index(1) do |score, index|
            #最終フレーム以外でのストライクなら、スコアにボーナスを含めて計算する
            if strike?(score) && not_last_frame?(index)
-               # 次のフレームもストライクで、なおかつ最終フレーム以外なら、
-               # もう一つ次のフレームの一投目をボーナスの対象にする
-               if strike?(@scores[index]) && not_last_frame?(index + 1)
-                   @total_score += 20 + @scores[index + 1].first
-                else
-                   @total_score += 10 + @scores[index].inject(:+)
-                end
+               @total_score += calc_strike_bonus(index)
            # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する
            elsif spare?(score) && not_last_frame?(index)
                 @total_score += calc_spare_bonus(index)
@@ -53,9 +47,14 @@ class Bowling
         score.inject(:+) == 10
     end
     
-    # 最終フレームで以外かどうか判定する
+    # 最終フレーム以外かどうか判定する
     def not_last_frame?(index)
        index < 10 
+    end
+   
+    # ストライクかどうか判定する
+    def strike?(score)
+        score.first == 10
     end
     
     # スペアボーナスを含んだ値でスコアを計算する
@@ -63,8 +62,14 @@ class Bowling
        10 + @scores[index].first 
     end
     
-    # ストライクかどうか判定する
-    def strike?(score)
-        score.first == 10
+    # ストライクボーナスを含んだ値でスコアを計算する
+    def calc_strike_bonus(index)
+        # 次のフレームもストライクで、なおかつ最終フレーム以外なら
+        # もう一つ次のフレームの一投目をボーナスの対象にする
+        if strike?(@scores[index]) && not_last_frame?(index + 1)
+            20 + @scores[index + 1].first
+        else
+            10 + @scores[index].inject(:+)
+        end
     end
 end
